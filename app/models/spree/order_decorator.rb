eb95a75c96cffd4d242fa9a9c8db521eba2c2d65
dach <daichi.yoshikawa1007@gmail.com>
2017-11-01 23:23:02 +0900

    メッセージカードなしを選択中はメッセージボックスは消失し、メッセージカードを選択すればメッセージカードが出現するようになりました

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,10 @@
 Spree::Order.class_eval do
   has_one :message, dependent: :destroy
 
-  accepts_nested_attributes_for :message
+  accepts_nested_attributes_for :message, reject_if: :reject_message
+
+  private
+    def reject_message(attributes)
+      attributes["message_card_id"].blank?
+    end
 end
---
+++ vendor/assets/javascripts/spree/frontend/message.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+  appearance =  new Vue({
+    el: '#message',
+    data: {
+      picked: ""
+    }
+  });
+};
