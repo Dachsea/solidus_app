c53058b2af94f4031c04f63171d2f14ef9d6dda4
dach <daichi.yoshikawa1007@gmail.com>
2017-11-01 23:45:59 +0900

    Step4完了

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -4,6 +4,8 @@ Spree::OrdersController.class_eval do
 
   def update
     if @order.contents.update_cart(order_params)
+      destroy_message_if_card_id_blank(order_params,@order)
+
       respond_with(@order) do |format|
         format.html do
           if params.has_key?(:checkout)
@@ -18,4 +20,12 @@ Spree::OrdersController.class_eval do
       respond_with(@order)
     end
   end
+
+  private
+    #メッセージカードを指定した後注文を途中でキャンセルし、「なし」を再度選択した場合には前回登録したメッセージが消える
+    def destroy_message_if_card_id_blank(params, order)
+      if params[:message_attributes][:message_card_id] == "" && !order.message.nil?
+        order.message.destroy
+      end
+    end
 end
